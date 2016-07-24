1701b9f0507970199a1009dc3870cc0561da1135
kdchk-Skylake <kdchk.net@gmail.com>
2016-07-24 13:35:13 +0300

    play with private fields and public properties

--- Temp-Test-Code/01_ConsoleApplication/Program.cs
+++ Temp-Test-Code/01_ConsoleApplication/Program.cs
@@ -10,9 +10,46 @@ namespace _01_ConsoleApplication
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Try to figure out with VS Extension and standalone GitHub app");
-            Console.ReadKey();
+            Worker worker = new Worker();
+            Console.WriteLine(worker.ConcatenatePublicStrings());
 
+            worker.FieldPublicText = "All your base are belong to us!";
+            Console.WriteLine($"worker.FieldPublicText = {worker.FieldPublicText}");
+            Console.WriteLine(worker.ConcatenatePublicStrings());
+
+            Console.WriteLine("--------");
+
+            Console.WriteLine(worker.ConcatenatePrivateStrings());
+
+            
+            Console.WriteLine($"worker.FieldPrivateText is {worker.FieldPrivateText}");
+
+
+
+            //---------------
+            Console.ReadKey();
+        }
+    }
+
+    class Worker
+    {
+        public string FieldPublicText = "class _Worker, field _public string _FieldPublicText";
+        private string fieldPrivateText = "class _Worker, field _PRIVATE string _fieldPrivateText";
+
+        public string FieldPrivateText { get { return fieldPrivateText; } }
+
+        public string ConcatenatePublicStrings()
+        {
+            string concatString = " +++ method _ConcatenatePublicStrings";
+            string answer = FieldPublicText + concatString;
+            return answer;
+        }
+
+        public string ConcatenatePrivateStrings()
+        {
+            string concatString = " +++ method _ConcatenatePrivateStrings";
+            string answer = fieldPrivateText + concatString;
+            return answer;
         }
     }
 }
